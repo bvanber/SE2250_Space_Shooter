3abe01971e57965dd727a81ed0b2dd485c609584
mmccor26 <43655058+mmccor26@users.noreply.github.com>
2019-02-16 13:44:25 -0500

    Enemies created

---
+++ bvanber2_Project_Phase1/Assets/_Scripts/Enemy.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [Header("Set in Inspector: Enemy")]
+    public float speed = 10f;
+    public float fireRate = 0.3f;
+    public float health = 10;
+    public int score = 100;
+    public Vector3 pos
+    {
+        get
+        {
+            return (this.transform.position);
+        }
+        set
+        {
+            this.transform.position = value;
+        }
+    }
+    public virtual void Move()
+    {
+        Vector3 tempPos = pos;
+        //gameObject.transform.Rotate(0,0,5);
+        
+        tempPos.y -= speed * Time.deltaTime;
+       
+        pos = tempPos;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+}
---
+++ bvanber2_Project_Phase1/Assets/_Scripts/Enemy_2.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_2 :  Enemy
+{
+    override public void Move()
+    {
+        Vector3 tempPos = pos;
+        gameObject.transform.Rotate(0,0,5);
+
+        tempPos.y -= speed * Time.deltaTime;
+
+        pos = tempPos;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.Move();
+        
+    }
+}
