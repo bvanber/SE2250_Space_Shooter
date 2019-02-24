aa3412692697fea8a06c49a8e6870ff454a21610
akettle4 <akettle4@uwo.ca>
2019-02-24 18:42:59 -0500

    made enemies list private

--- bvanber2_Project_Phase1/Assets/__Scripts/Main.cs
+++ bvanber2_Project_Phase1/Assets/__Scripts/Main.cs
@@ -7,7 +7,7 @@ public class Main : MonoBehaviour
     public GameObject[] prefabEnemies = new GameObject[3];
     private float _timer;
     private Vector3 _startPos;
-    public List<GameObject> _enemies = new List<GameObject>();
+    private List<GameObject> _enemies = new List<GameObject>();
     private List<GameObject> _toDelete = new List<GameObject>();
    
     // Start is called before the first frame update
@@ -28,7 +28,7 @@ public class Main : MonoBehaviour
             int num = rand.Next()%3;
             //if enemy 0 or 1 start above the view with a random x value otherwise start with a random y value if enemy 2
             if (num == 0 || num == 1) { _startPos = new Vector3(Random.Range(-45, 45), 60, 0); }
-            else { _startPos = new Vector3(50, Random.Range(-45, 60), 0); }
+            else { _startPos = new Vector3(50, Random.Range(-20, 60), 0); }
 
             GameObject enn= Instantiate(prefabEnemies[(num) % 3],_startPos, Quaternion.identity);
             _enemies.Add(enn);
